80fc58ad68ee32abcc7124498ac6dc0a98e2c04e
myrinw <myrin.w@outlook.com>
2020-08-13 12:39:13 +0200

    Added a footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import './App.css';
 
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
+import { Switch, Route } from 'react-router-dom';
 
+const home = () => <div>Welcome home bitch</div>
+const forum = () => <div>Welcome to the forum bitch</div>
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Switch>
+        <Route exact path="/" component={home} />
+        <Route path="/forum" component={forum} />
+      </Switch>
+      <Footer />
     </div>
   );
 }
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import './Navbar.scss';
 
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function Navbar() {
     return <div>
         <div className="top-bar">
-            <p>tastebook</p>
+            <div className="container">
+                <div className="space-between">
+                    <h2>Tastebook</h2>
+                    <FontAwesomeIcon icon={faUser} size="2x" />
+                </div>
+            </div>
+
         </div>
         <div className="navigation-bar">
             <Link to="/">Home</Link>
