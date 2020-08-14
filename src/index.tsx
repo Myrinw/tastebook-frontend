0668874f03aa91e37d579c9620803d230e517af2
myrinw <myrin.w@outlook.com>
2020-08-14 10:42:08 +0200

    redux store is created

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
-//import { Provider } from "react-redux";
-//mport {blabla} from "./store";
+import { Provider } from "react-redux";
+import store from "./store";
 
 
 
 ReactDOM.render(
-
-  <Router >
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router >
+      <App />
+    </Router>
+  </Provider>,
 
   document.getElementById('root')
 );
---
+++ src/store/recucer.js
@@ -0,0 +1,7 @@
+import { combineReducer } from 'redux';
+
+const reducer = combineReducer({
+
+})
+
+export default reducer;
